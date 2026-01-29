7bffd2960a8774c54d97522d336e526fb7bfbacf
Justet <dimfisl33t@gmail.com>
2026-01-29 20:04:58 +0700

    Добавлена валидация.

--- src/main/java/ru/shift/userimporter/api/controller/UserController.java
+++ src/main/java/ru/shift/userimporter/api/controller/UserController.java
@@ -1,9 +1,12 @@
 package ru.shift.userimporter.api.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.shift.userimporter.api.ApiPath;
 import ru.shift.userimporter.api.dto.UserResponse;
 import ru.shift.userimporter.api.mapper.UserMapper;
 import ru.shift.userimporter.core.model.UserFilter;
@@ -11,15 +14,16 @@ import ru.shift.userimporter.core.service.UserService;
 
 import java.util.List;
 
+@Validated
 @RestController
-@RequestMapping("/clients")
+@RequestMapping(ApiPath.clients)
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
     @GetMapping
-    public List<UserResponse> getUsers(UserFilter filter) {
+    public List<UserResponse> getUsers(@Valid UserFilter filter) {
         return userService.getUsers(filter)
                 .stream()
                 .map(userMapper::toResponse)
